5883b8667541d608d1ec38e69a1315dbcfc44903
Jakub Werwinski <werwa999@wp.pl>
2023-12-03 17:25:05 +0100

    first day

---
+++ day 1/firstday.py
@@ -0,0 +1,14 @@
+file_path = "input.in"
+
+sum = 0
+
+with open(file_path, "r") as file:
+    for line in file:
+        words = line.split()
+        for word in words:
+            numbers = [letter for letter in word if letter.isdigit()]
+            if numbers:
+                extnum = int(numbers[0] + numbers[-1])
+                sum += extnum
+
+print(sum)
